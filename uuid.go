0ba9c44b8f221620175b5f18bf94ecd3219a3486
heisonyee <heisonyee@126.com>
2023-10-19 09:41:38 +0800

    Fix

--- uuid.go
+++ uuid.go
@@ -2,9 +2,15 @@ package watermill
 
 import (
 	"github.com/google/uuid"
+	"github.com/lithammer/shortuuid/v4"
 )
 
 // NewUUID returns a new UUID Version 4.
 func NewUUID() string {
 	return uuid.New().String()
+}
+
+// NewShortUUID returns a new short UUID.
+func NewShortUUID() string {
+	return shortuuid.New()
 }
